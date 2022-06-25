eae41dab97339151658f46a4f8e336fbb57990e1
Jack Kellenberger <jackson@kellenberger.io>
2022-06-25 15:09:07 -0400

    Implement game history, clean up results display, error handling for misclicks

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,22 +1,56 @@
 const score = {
     player: 0,
     computer: 0,
+    GamesWon: 0,
+    GamesLost: 0,
 }
 const buttons = document.querySelectorAll('button');
-const results = document.getElementById('results')
+const results = document.getElementById('results');
+const currentScore = document.getElementById('currentScore');
+const history = document.getElementById('history');
 
-function displayResults(result) {
-    let roundResult = document.createElement('h2');
+currentScore.textContent = `Current Score: ${score.player}-${score.computer}`
+
+function displayResults(result, pChoice, cChoice) {
+    let roundResult = document.createElement('h3');
+    let choices = document.createElement('p');
     let outcomes = {
-        'win': `You won the round!<br>Current score:<br>
-                You: ${score.player}<br>Comp:${score.computer}`,
-        'loss': `You lost the round!<br>Current score:<br>
-                You: ${score.player}<br>Comp:${score.computer}`,
-        'tie': `You tied the round!<br>Current score:<br>
-                You: ${score.player}<br>Comp:${score.computer}`
-    }    
-    roundResult.innerHTML = outcomes[result];
-    results.appendChild(roundResult)
+        'win': 'You won the round!',
+        'loss': 'You lost the round!',
+        'tie': 'You tied the round!',
+        'matchwon': "You were the first to five! You WIN!",
+        'matchlost': "The computer was the first to five! You LOSE!"
+    }
+    if (score.player < 5 && score.computer < 5) {
+        roundResult.innerHTML = outcomes[result];
+        results.appendChild(roundResult)
+        currentScore.textContent = 
+        `Current Score: ${score.player} - ${score.computer}`;
+    }
+    else if (score.player = 5) {
+        roundResult.innerHTML = outcomes['matchwon'];
+        results.appendChild(roundResult);
+        ++score.GamesWon;
+        history.textContent =
+                 `Games won: ${score.GamesWon} Games lost: ${score.GamesLost}`;
+        score.player = 0;
+        score.computer = 0;
+        currentScore.textContent = 
+        `Current Score: ${score.player} - ${score.computer}`;
+    }
+    else {
+        roundResult.innerHTML = outcomes['matchlost'];
+        results.appendChild(roundResult);
+        ++score.GamesLost;
+        history.textContent =
+                 `Games won: ${score.GamesWon} Games lost: ${score.GamesLost}`;
+        score.player = 0;
+        score.computer = 0;
+        currentScore.textContent = 
+        `Current Score: ${score.player} - ${score.computer}`;
+    }
+    choices.textContent = `You played ${pChoice} against ${cChoice}!`
+    results.appendChild(choices);
 }
 
 function playRound(e) {
@@ -24,10 +58,9 @@ function playRound(e) {
     let result = null;
     let pChoice = e.target.id;
     let cChoice = choices[Math.floor(Math.random() * choices.length)]
-    results.innerHTML = ''
-    console.log(`Player picked: ${pChoice}`)
-    console.log(`Computer picked: ${cChoice}`);
     
+    if (!choices.includes(pChoice)) return;
+
     if (pChoice === cChoice) result = 'tie';
     else if (pChoice === 'rock' && cChoice === 'scissors' ||
             pChoice === 'paper' && cChoice === 'rock' ||
@@ -39,8 +72,9 @@ function playRound(e) {
         ++score.computer;
         result = 'loss';   
     }
-    displayResults(result);
-    console.log(`Current Score: ${score.player} to ${score.computer}.`)
+    results.innerHTML = ''
+    displayResults(result, pChoice, cChoice);
 };
 
 buttons.forEach(button => addEventListener('click', playRound));
+
