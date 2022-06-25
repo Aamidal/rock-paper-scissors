81d60cac9206056d038dcf001c1d571695516ada
Jack Kellenberger <jackson@kellenberger.io>
2022-06-25 14:39:19 -0400

    Implement dynamic result display

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,27 +3,43 @@ const score = {
     computer: 0,
 }
 const buttons = document.querySelectorAll('button');
-console.log(buttons);
+const results = document.getElementById('results')
+
+function displayResults(result) {
+    let roundResult = document.createElement('h2');
+    let outcomes = {
+        'win': `You won the round!<br>Current score:<br>
+                You: ${score.player}<br>Comp:${score.computer}`,
+        'loss': `You lost the round!<br>Current score:<br>
+                You: ${score.player}<br>Comp:${score.computer}`,
+        'tie': `You tied the round!<br>Current score:<br>
+                You: ${score.player}<br>Comp:${score.computer}`
+    }    
+    roundResult.innerHTML = outcomes[result];
+    results.appendChild(roundResult)
+}
 
 function playRound(e) {
     const choices = ['rock', 'paper', 'scissors'];
-    // console.log(typeof e.target.id);
+    let result = null;
     let pChoice = e.target.id;
     let cChoice = choices[Math.floor(Math.random() * choices.length)]
-
+    results.innerHTML = ''
     console.log(`Player picked: ${pChoice}`)
     console.log(`Computer picked: ${cChoice}`);
     
-    if (pChoice === cChoice) console.log("You tied!");
+    if (pChoice === cChoice) result = 'tie';
     else if (pChoice === 'rock' && cChoice === 'scissors' ||
             pChoice === 'paper' && cChoice === 'rock' ||
             pChoice === 'scissors' && cChoice === 'paper') {
-                console.log("You won!");
                 ++score.player
+                result = 'win';
             }
-    else {console.log("You lost!");
-        ++score.computer;}
-
+    else {
+        ++score.computer;
+        result = 'loss';   
+    }
+    displayResults(result);
     console.log(`Current Score: ${score.player} to ${score.computer}.`)
 };
 
